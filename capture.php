f19e6a616103facab5cf46c0e03ec2e5ff892ad5
Iniubong Obonguko <iniubongobonguko2018@gmail.com>
2020-07-17 00:37:44 +0100

    made new files

--- capture.php
+++ capture.php
@@ -2,12 +2,15 @@
 $connection = mysqli_connect('localhost', 'root','', 'email-list');
 
 if(isset($_POST['submit'])){
-    // $userName = $_POST['name'];
-    // $userName = mysqli_real_escape_string($connection, $userName);
     $userEmail = $_POST['email'];
     $userEmail = mysqli_real_escape_string($connection, $userEmail);
 
     $query = "INSERT INTO capture (email) VALUES ('$userEmail')";
 
     $result = mysqli_query($connection, $query);
+  
+    echo "<script type='text/javascript'>
+   window.location.pathname = 'landingPage' </script>";
+
+    
 }
---
+++ javascript/app.js
@@ -0,0 +1,9 @@
+
+// var email = $("#email");
+// $("#submit").click(function (e) {
+//   $.ajax({
+//     type: "POST",
+//     data: { email: email.val() },
+//     url: "../capture.php",
+//   });
+// });
