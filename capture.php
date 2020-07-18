5ef33437d84e18ac653070665ced460c7d636b10
Iniubong Obonguko <iniubongobonguko2018@gmail.com>
2020-07-18 22:10:42 +0100

    initial commits

--- capture.php
+++ capture.php
@@ -1,16 +1,18 @@
 <?php
-$connection = mysqli_connect('localhost', 'root','', 'email-list');
+$connection = mysqli_connect("localhost", "id14348677_emailcapture","4p!c(fdOwqSndl[-", "id14348677_capture");
 
 if(isset($_POST['submit'])){
+   
     $userEmail = $_POST['email'];
     $userEmail = mysqli_real_escape_string($connection, $userEmail);
 
-    $query = "INSERT INTO capture (email) VALUES ('$userEmail')";
+    $query = "INSERT INTO useremails (email) VALUES ('$userEmail')";
 
     $result = mysqli_query($connection, $query);
-  
+    
     echo "<script type='text/javascript'>
    window.location.pathname = 'landingPage' </script>";
 
-    
 }
+
+    
--- javascript/app.js
+++
@@ -1,9 +0,0 @@
-
-// var email = $("#email");
-// $("#submit").click(function (e) {
-//   $.ajax({
-//     type: "POST",
-//     data: { email: email.val() },
-//     url: "../capture.php",
-//   });
-// });
